ed92a59f5ef305ea3b461a3974909af158f7c5cf
Christopher Booker <cryanb91@gmail.com>
2018-06-15 22:27:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Quick-Pass
+
+Quck-Pass is a Python script that generates a strong
+password that can be used for just about anything.
+It sucks trying to think of a new password on the fly
+Quick-Pass takes care of that ickyness for you.  
+
+
+
+
---
+++ quickpass.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+"""
+import argparse 
+import random
+import string
+
+def get_args():
+    """
+    """
+    pass
+
+def generate_pass():
+    """
+    """
+    pass
+
+def main():
+    """
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
