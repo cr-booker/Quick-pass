a1a5e42b450ff4d818db6b3d17dfb5ae630be52a
Christopher Booker <cryanb91@gmail.com>
2018-09-18 19:21:59 -0400

    Trimmed Module Docstring

--- quickpass.py
+++ quickpass.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 """
-Quck-Pass is a Python script that
+Quick-Pass is a Python script that
 generates a strong password or passphrase 
 that can be used for just about anything.
-
-It sucks trying to think of a new
-password on the fly,
-Quick-Pass takes care of that ickyness for you.
 """
 import argparse 
 try:
